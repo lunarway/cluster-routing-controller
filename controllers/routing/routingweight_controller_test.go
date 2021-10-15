7e921fa50c476b7e25821952b0aacc021645580b
Martin Anker Have <mah@lunar.app>
2021-10-15 08:59:04 +0200

    sort imports

--- controllers/routing/routingweight_controller_test.go
+++ controllers/routing/routingweight_controller_test.go
@@ -7,12 +7,8 @@ import (
 
 	"github/lunarway/cluster-routing-controller/apis/routing/v1alpha1"
 
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	corev1 "k8s.io/api/core/v1"
-
 	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -20,6 +16,8 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
 
 func TestRoutingWeightController(t *testing.T) {
